f2666bf2271566329ece438d47312aade099ee98
Henrique-M-Rodrigues <hmr.ads.fepi@gmail.com>
2023-06-14 22:57:17 -0300

    desafio

--- desafios/virtualfone/js/main.js
+++ desafios/virtualfone/js/main.js
@@ -1,29 +1,46 @@
-
-//Pega os valores de todos os botões
-const listaTeclas = document.querySelectorAll('input[type=button]');
-const display =  document.querySelector('input[type=tel]');
-
-function valorBotao(valor){
-    display.value = display.value + listaTeclas[valor].value;
+function valorPrimeiroBotao() {
+    alert(document.querySelector
+        ('input[type=button]').value);
 }
 
-function limpaDisplay(){
-    display.value = '' // coloca o valor do display como vazio
+const listaTeclas = document.querySelectorAll
+    ('input[type=button]');
+const display = document.querySelector('input[type=tel]');
+
+function valorBotaoUmJanela() {
+    alert(listaTeclas[0].value);
 }
 
-// let i = 0;
-// while (i < listaTeclas.length){
-//     const tecla = listaTeclas[i];
-//     const indice = i
-//     tecla.onclick = function(){
-//         valorBotao(indice);     
-//     }
-//     i++;
-// }
+function valorBotaoUm() {
+    display.value += listaTeclas[0].value;
+}
 
-for (let i = 0; i < listaTeclas.length; i++){
-        const tecla = listaTeclas[i];
-        tecla.onclick = function(){
-        valorBotao(i);     
+function valorBotao(valor) {
+    if (display.value.length < 8 && listaTeclas[valor].value !== '#') {
+        display.value += listaTeclas[valor].value;
+    } else {
+        alert('Fora do limite permitido ou função não liberada.')
     }
+
+}
+
+/* let i = 0;
+while(i < listaTeclas.length){
+    const tecla = listaTeclas[i];
+    const indice = i;
+    tecla.onclick = function(){
+        valorBotao(indice);
+    }
+    i = i + 1;
+}  */
+
+for (let index = 0; index < listaTeclas.length; index++) {
+    const tecla = listaTeclas[index];
+    tecla.onclick = () => {
+        valorBotao(index);
+    }
+}
+
+function limpar() {
+    display.value = '';
 }
